a859b2d9c90e16f9ec3d54ac65fccf428dd45dea
putridresty <putridresty25@gmail.com>
2022-10-13 21:36:47 +0700

    update js css

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-let api_key = "21b6ea03e3c7eabe9205a2d84d62368a";
-
 
 let getDataMovies = async () => {
-  
-  let containerData = document.getElementById("row");
+    let api_key = "21b6ea03e3c7eabe9205a2d84d62368a";
+    let img_URL = "https://image.tmdb.org/t/p/w500/";
+    let response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key="+api_key+"&sort_by=popularity.desc&page=1"
+    );
+    let movies = await response.json();
+    const remove = (sel) => document.querySelectorAll(sel).forEach((el) => el.remove());
+        remove(".isiFilm");
+    let dataMovies = movies.results;
 
-  let response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1`
-  );
-  let movies = await response.json();
-  let dataMovies = movies.results;
-
-  dataMovies.forEach((item) => {
-    console.log(item);
-
-    containerData.innerHTML += `
-            <div class="col-lg-4 col-xxl-3 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="./img/img.png" alt="no-image" id="movie-img">
+    dataMovies.forEach((item) => {
+        let containerData = document.getElementById("row");
+        containerData.innerHTML += `
+            <div class="col-lg-4 col-xxl-3 mb-3 isiFilm">
+                <div class="card h-100">
+                    <img src="` + img_URL +``+ item.poster_path +`" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 id="card-title">${item.title}</h3>
                             <p class="card-text" id="vote_averange"></i>${item.vote_average}</p>
@@ -26,50 +24,44 @@ let getDataMovies = async () => {
                 </div>
             </div>
             `;
-  });
+    });
 };
 getDataMovies();
 
 
-function search_data() {
-  document.querySelector("form").addEventListener("submit", function () {
-      // ev.preventDefault();
-      let containerSearch = document.getElementById("row");
-  
-      let searchInput = document.querySelector("input").value;
-      // let buttonSearch = document.addEventListener
-  
-      // let containerSearch = document.getElementById("row")
-      // containerSearch.innerHTML = `<p>${searchInput}</p>`
-  
-      // ambil data
-      let dataSearch = async (inputUser) => {
-          let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${inputUser}&page=1`)
-          let movies = await response.json()
-          // console.log(movies.results)
-          let dataMovies = movies.results;
-  
-  
-          dataMovies.forEach((item) => {
-              // console.log(item);
-  
-              containerSearch.innerHTML += `
-                      <div class="col-lg-4 col-xxl-3 mb-3">
-                          <div class="card">
-                              <img class="card-img-top" src="./img/img.png" alt="no-image" id="movie-img">
-                                  <div class="card-body">
-                                      <h3 id="card-title">${item.title}</h3>
-                                      <p class="card-text" id="vote_averange"></i>${item.vote_average}</p>
-                                      <p class="card-text" id="release_date">${item.release_date}</p>
-                                  </div>
-                          </div>
-                      </div>
-                      `;
-          });
-  
-      }
-      dataSearch(searchInput)
-  })
-  
-}
 
+function setName() {
+    let search = document.getElementById("input-search");
+    let setSearch = search.value;
+
+    let api_key = "21b6ea03e3c7eabe9205a2d84d62368a";
+    const search_URL = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${setSearch}&page=1`;
+    const img_URL = "https://image.tmdb.org/t/p/w500/";
+    
+    
+    fetch(search_URL)
+        .then((response) => response.json())
+        .then((movies) => {
+        let containerData = document.getElementById("row");
+        const remove = (sel) => document.querySelectorAll(sel).forEach((el) => el.remove());
+            remove(".isiFilm");
+            let dataMovies = movies.results;
+        
+            dataMovies.forEach((item) => {
+                // let containerData = document.getElementById("row");
+                containerData.innerHTML += `
+                    <div class="col-lg-4 col-xxl-3 mb-3 isiFilm">
+                        <div class="card h-100">
+                            <img src="` + img_URL +``+ item.poster_path +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h3 id="card-title">${item.title}</h3>
+                                    <p class="card-text" id="vote_averange"></i>${item.vote_average}</p>
+                                    <p class="card-text" id="release_date">${item.release_date}</p>
+                                </div>
+                        </div>
+                    </div>
+                    `;
+            });
+
+    })
+}
